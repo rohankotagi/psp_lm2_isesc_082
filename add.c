f74660ff75049800f1dd82aea689beb4a388e49b
rohankotagi <110719292+rohankotagi@users.noreply.github.com>
2022-08-06 16:15:42 +0530

    Completed Addition of Two numbers file commit

--- add.c
+++ add.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+  int a, b, sum;
+  printf(" Enter 2 numbers to be added");
+  scanf("%d %d" , &a, &b);
+  sum= a+b;
+  printf("Sum of %d and %d is %d", a, b, sum);
+  return 0;
+}
+  
